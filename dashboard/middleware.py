76678b4d50443f1b8ed6fb4fc832cbe55c9d4e19
HamzaOPLEX <hamouzaala2019@gmail.com>
2021-03-22 12:59:39 +0100

    Fix *CheckSetting* Middelware Login Issue

--- dashboard/middleware.py
+++ dashboard/middleware.py
@@ -7,9 +7,7 @@ class CheckCompanySettingMiddelWare:
     def __init__(self, get_response):
         self.get_response = get_response
     def __call__(self,request):
-        print(request)
         ALLOWED_URLS = ['/settings/','/settings/global-settings', '/login/', '/logout/']
-        print(request.path)
         if request.path in ALLOWED_URLS:
             response = self.get_response(request)
         if request.path not in ALLOWED_URLS:
