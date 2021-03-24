bce197242f62e6eda2ae10e4aa39773d3c67cdef
Hamza Alaoui Hamdi <53179016+HamzaOPLEX@users.noreply.github.com>
2021-03-24 22:12:10 +0100

    Fix middelware issue

--- dashboard/middleware.py
+++ dashboard/middleware.py
@@ -1,17 +1,34 @@
 from pprint import pprint
 from dashboard.models import APP_Settings
 from django.shortcuts import HttpResponse,redirect
-
+import re
 
 class CheckCompanySettingMiddelWare:
     def __init__(self, get_response):
         self.get_response = get_response
     def __call__(self,request):
-        ALLOWED_URLS = ['/settings/', '/settings/global-settings',
-                        '/login/', '/logout/', '/login', '/logout','/admin','/admin/']
-        if request.path in ALLOWED_URLS:
+        ALLOWED_URLS = [
+                            '/settings', 
+                            '/login', 
+                            '/logout',
+                            '/admin'
+                        ]
+        for path in ALLOWED_URLS :
+            # for each allowed url create a regex to match evry think that come after it
+            path = path + '.+'
+            regx = re.compile(path)
+            try : 
+                regx = regx.search(request.path).group(0)
+                if regx:
+                    bypass_check = True
+                    break
+            except Exception:
+                bypass_check = False
+
+
+        if bypass_check == True:
             response = self.get_response(request)
-        if request.path not in ALLOWED_URLS:
+        if bypass_check == False:
             company_settings = APP_Settings.objects.all().first()
             if company_settings:
                 response = self.get_response(request)
