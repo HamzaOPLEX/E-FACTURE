82e0efa3233eb8b6ef11ca86ba2787474f481b9c
Hamza Alaoui Hamdi <53179016+HamzaOPLEX@users.noreply.github.com>
2021-03-29 13:44:39 +0200

    Update middleware.py

--- dashboard/middleware.py
+++ dashboard/middleware.py
@@ -31,7 +31,7 @@ class CheckCompanySettingMiddelWare:
         if bypass_check == False:
             try :
                 company_settings = APP_Settings.objects.all().first()
-            except Exception:
+            except ProgrammingError:
                 # if table does not exist just set var as false
                 company_settings = False
             if company_settings:
